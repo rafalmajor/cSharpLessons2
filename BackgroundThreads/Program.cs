2380608a68d6db379a0e963557d0b8de558f869a
rafalmajor <rafal.major@gmail.com>
2019-12-11 21:51:28 +0100

    Code for tasks added

--- BackgroundThreads/Program.cs
+++ BackgroundThreads/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace BackgroundThreads
 {
     class Program
     {
+        public async static Task StartTask(int index)
+        {
+            await SortAsync(index).ConfigureAwait(true);
+        }
+
+        public static Task SortAsync(int index)
+        {
+            return Task.Factory.StartNew(
+                () => { Console.WriteLine($"Task {index}"); });
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i < 11; i++)
+            {
+                StartTask(i);
+            }
+
+            Console.WriteLine("End of Hello World!");
         }
     }
 }
